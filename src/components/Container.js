01f55a85cdc757e46fcf9986f982848a6d0b23bd
B-MNSN <manutsanan0@gmail.com>
2022-09-19 23:50:00 +0700

    add container

---
+++ src/components/Container.js
@@ -0,0 +1,13 @@
+import propType from 'prop-types';
+
+function Container({children}){
+    return  (
+        <main>{children}</main>
+    );
+}
+
+Container.prototype = {
+    children: propType.node.isRequired
+}
+
+export default Container;
