d1d7e41e0830616ff2033e7aff84a21a920ebe1c
B-MNSN <manutsanan0@gmail.com>
2022-09-21 23:15:22 +0700

    test home

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Navbar from './components/Navbar';
 // import Login from './components/Login';
 // import Signup from './components/Signup';
 import Container from './components/Container';
-import Flight from './components/Flight';
-import CardSelectFlight from './components/CardsSelectFlight';
+// import Flight from './components/Flight';
+// import CardSelectFlight from './components/CardsSelectFlight';
 import Home from './pages/Home';
 
 function App() {
