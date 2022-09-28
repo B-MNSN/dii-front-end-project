b5d42869c502ddeb59fe72c04ae17a6ea04afb17
AlpacaUniix <supatsara_wan@cmu.ac.th>
2022-09-28 23:44:07 +0700

    reset branch

---
+++ src/components/OptionalReturn.js
@@ -0,0 +1,26 @@
+function OptionalReturn({ books }) {
+    if (books.return?.price) {
+        return (<div className="col-11 mx-4 mt-3">
+            <div className="col d-flex justify-content-between">
+                <h6 className="fw-bold my-1">เที่ยวบินขากลับ</h6>
+                <p className="fw-bold my-1">฿{books.return?.price}</p>
+            </div>
+            <ul className="col p-0 ms-3">
+                <li className="my-2 d-flex justify-content-between">
+                    <text>ผู้ใหญ่x{books.traveler}</text>
+                    <text className="">{books.traveler * books.return?.price}</text>
+                </li>
+                <li className="my-2 d-flex justify-content-between">
+                    <text>ภาษี</text>
+                    <text className="">-</text>
+                </li>
+                <li className="my-2 d-flex justify-content-between">
+                    <text>ส่วนลด</text>
+                    <text className="">-</text>
+                </li>
+            </ul>
+        </div>);
+    }
+}
+
+export default OptionalReturn;
