ff7ca691835c482c96d076bf598306d9f55a6a2c
AlpacaUniix <supatsara_wan@cmu.ac.th>
2022-09-30 22:33:50 +0700

    fix error

--- src/components/MyBooking.jsx
+++ src/components/MyBooking.jsx
@@ -34,49 +34,49 @@ function MyBooking() {
           <Card.Title className="">
             <strong>ข้อมูลผู้โดยสาร</strong>{" "}
           </Card.Title>
-          <Card.Text>
-            <p className="mb-3" style={{ color: "black" }}>
+          <>
+            <div className="mb-3" style={{ color: "black" }}>
               <strong>Mrs Elsa Frozen</strong>{" "}
-            </p>
+            </div>
             <footer className="blockquote-footer" style={{ color: "#5F6F94" }}>
               Ticket Number : 1234566789
             </footer>
-          </Card.Text>
+          </>
           <Card.Title>
             <h5>
               <strong>ข้อมูลติดต่อ</strong>
             </h5>
           </Card.Title>
-          <Card.Text>
+          <>
             <div className="container-footer">
               <div className="row align-items-end">
                 <div className="col order-first">
-                  <p className="mb-3" style={{ color: "#5F6F94" }}>
+                  <div className="mb-3" style={{ color: "#5F6F94" }}>
                     <small>ชื่อ-นามสกุล</small>{" "}
-                  </p>
+                  </div>
                   <footer className="blockquote-footer" style={{ color: "black" }}>
                     <strong>Elsa Frozen</strong>
                   </footer>
                 </div>
                 <div className="col">
-                  <p className="mb-3" style={{ color: "#5F6F94" }}>
+                  <div className="mb-3" style={{ color: "#5F6F94" }}>
                     <small>อีเมล</small>{" "}
-                  </p>
+                  </div>
                   <footer className="blockquote-footer" style={{ color: "black" }}>
                     <strong>elsa_frozen@email.com</strong>
                   </footer>
                 </div>
                 <div className="col order-last">
-                  <p className="mb-3" style={{ color: "#5F6F94" }}>
+                  <div className="mb-3" style={{ color: "#5F6F94" }}>
                     <small>เบอร์โทรศัพท์</small>
-                  </p>
+                  </div>
                   <footer className="blockquote-footer" style={{ color: "black" }}>
                     <strong>+66(0)89-789-1234</strong>
                   </footer>
                 </div>
               </div>
             </div>
-          </Card.Text>
+          </>
         </Card.Body>
       </Card>
     </>
