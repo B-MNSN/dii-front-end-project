ab6983f66fb27fe2cab498ec50357817a2bc157b
B-MNSN <manutsanan0@gmail.com>
2022-10-01 04:47:34 +0700

    Edit fs

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -9,41 +9,41 @@ function Ticket({ show, onHide, book, flight }) {
             <Modal {...propsSim} size="xl" aria-labelledby="contained-modal-title-vcenter" centered>
                 <Modal.Header closeButton className='topModal bg-opacity-10 btn-light'>
                     <Modal.Title id="contained-modal-title-vcenter">
-                        <div className='ms-5 d-flex align-items-center '>
+                        <div className='d-flex align-items-center '>
                             <img src={logo} alt='logo' width={80} />
                             {/* <h4 className='ms-3 mt-1 mb-0 fw-bold'>BUN-AL</h4> */}
                         </div>
                     </Modal.Title>
                 </Modal.Header>
-                <Modal.Body className="ticket">
+                <Modal.Body className="ticket rounded-3">
                     <div className='row mx-5 my-2'>
                         <div className='col-12 mb-4'>
                             <text className='me-3 fw-bold text-uppercase fs-5'>Name:</text>
-                            <text className='fs-6'>{book?.fname} {book?.lname}</text>
+                            <text className='fs-5'>{book?.fname} {book?.lname}</text>
                         </div>
                         <div className='col-6 mb-4'>
                             <text className='me-3 fw-bold text-uppercase fs-5'>From:</text>
-                            <text className='fs-6'>{flight?.departLocate}</text>
+                            <text className='fs-5'>{flight?.departLocate}</text>
                         </div>
                         <div className='col-6 mb-4'>
                             <text className='me-3 fw-bold text-uppercase fs-5'>To:</text>
-                            <text className='fs-6'>{flight?.landLocate}</text>
+                            <text className='fs-5'>{flight?.landLocate}</text>
                         </div>
                         <div className='col-4 mb-4'>
                             <text className='me-3 fw-bold text-uppercase fs-5'>Flight:</text>
-                            <text className='fs-6'>{flight?.flightCode}</text>
+                            <text className='fs-5'>{flight?.flightCode}</text>
                         </div>
                         <div className='col-4 mb-4'>
                             <text className='me-3 fw-bold text-uppercase fs-5'>Date:</text>
-                            <text className='fs-6'>{flight?.departDay}</text>
+                            <text className='fs-5'>{flight?.departDay}</text>
                         </div>
                         <div className='col-4 mb-4'>
                             <text className='me-3 fw-bold text-uppercase fs-5'>Seat:</text>
-                            <text className='fs-6'>1A</text>
+                            <text className='fs-5'>1A</text>
                         </div>
                         <div className='col-4 mb-4'>
                             <text className='me-3 fw-bold text-uppercase fs-5'>Boarding Time:</text>
-                            <text className='fs-6'>{flight?.departTime}</text>
+                            <text className='fs-5'>{flight?.departTime}</text>
                         </div>
                     </div>
 
