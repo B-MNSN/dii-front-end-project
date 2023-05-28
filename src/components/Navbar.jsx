32fa0a522e6cf00145202df7103c35b1d4a99a0b
B-MNSN <manutsanan0@gmail.com>
2023-05-28 22:40:14 +0700

    install stye component and edit icon user

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { FaUserAlt } from 'react-icons/fa';
+import { FaUserCircle } from 'react-icons/fa';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from '../logo.png';
 
 
-function BasicExample() {
+
+function BasicExample({ className }) {
   function out() {
     localStorage.clear();
     window.location.href = '/';
@@ -21,7 +22,7 @@ function BasicExample() {
             <Nav.Link className='text-light fw-bold' href="/home">Home</Nav.Link>
             <Nav.Link className='text-light fw-bold' href="/MyBooking">My Booking</Nav.Link>
             <Nav.Link className='text-light fw-bold' href="#">Help</Nav.Link>
-            <NavDropdown className='' align="end" id="basic-nav-dropdown" title=<FaUserAlt size={'25'} color={'#ffffff'} />>
+            <NavDropdown className={ className } align="end" title=<FaUserCircle size={'25'} color={'#ffffff'} />>
               <NavDropdown.Item onClick={out} href='#'>Singout</NavDropdown.Item>
             </NavDropdown>
           </Nav>
