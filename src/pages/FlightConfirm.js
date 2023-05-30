a5a8c96fa8bcf22151406ace9eb2abc2f9db06de
B-MNSN <manutsanan0@gmail.com>
2023-05-31 02:23:23 +0700

    dispatch fecthFlight

--- src/pages/FlightConfirm.js
+++ src/pages/FlightConfirm.js
@@ -6,17 +6,23 @@ import PriceDetails from '../components/PriceDetails';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+import { fetchFlights } from '../reducers/actions';
+import { useSelector, useDispatch } from 'react-redux';
 
 
 function FlightConfirm() {
     let userStat = JSON.parse(localStorage.getItem('status'));
-    const [flights, setFlights] = useState([]);
+    // const [flights, setFlights] = useState([]);
+    const flights = useSelector((state) => state.flights);
+    const dispatch = useDispatch();
+  
     useEffect(() => {
         async function getFlights() {
             const flights = await axios.get(
                 `http://localhost:8000/flights${window.location.search}`
             );
-            setFlights(flights.data[0]);
+            // setFlights(flights.data[0]);
+            dispatch(fetchFlights(flights.data[0]))
 
         }
         getFlights();
@@ -26,7 +32,8 @@ function FlightConfirm() {
         return (
             <>
                 <Navbar />
-                <Container>
+                {flights ? (
+                    <Container>
                     <MyFlight flight={flights} />
                     <div className='d-flex justify-content-center w-75'>
                         <div className='row '>
@@ -39,6 +46,10 @@ function FlightConfirm() {
                         </div>
                     </div>
                 </Container>
+                ) : (
+                    <div>Loading Flighgts...</div>
+                )}
+
             </>
         );
     } else {
